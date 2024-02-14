d6f337a8209136ea48f397d383f4e2d30c313e46
bDavidKrismar <132487881+bDavidKrismar@users.noreply.github.com>
2024-02-14 12:29:03 -0600

    primer commit

---
+++ reconocimientoVoz.js
@@ -0,0 +1,57 @@
+document.getElementById('start-btn').addEventListener('click', function() {
+    if ('webkitSpeechRecognition' in window) {
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = false;
+        recognition.lang = 'es-ES';
+        
+        recognition.onstart = function() {
+            // Muestra el indicador de escucha
+            document.getElementById('listening-indicator').style.display = 'block';
+        };
+
+        recognition.onresult = function(event) {
+            var colorHeard = event.results[0][0].transcript.trim().toLowerCase();
+            changeBackgroundColor(colorHeard);
+        };
+
+        recognition.onend = function() {
+            // Oculta el indicador de escucha
+            document.getElementById('listening-indicator').style.display = 'none';
+        };
+
+        recognition.onerror = function(event) {
+            console.error('Error en el reconocimiento de voz: ', event.error);
+            // Asegúrate de ocultar el indicador si hay un error
+            document.getElementById('listening-indicator').style.display = 'none';
+        };
+
+        recognition.start();
+    } else {
+        alert('Tu navegador no soporta webkitSpeechRecognition');
+    }
+});
+
+
+function changeBackgroundColor(color) {
+    // Mapeo de colores en español a inglés
+    var colorMap = {
+        'rojo': 'red',
+        'verde': 'green',
+        'azul': 'blue',
+        'amarillo': 'yellow',
+        'naranja': 'orange',
+        'negro': 'black',
+        'blanco': 'white',
+        'morado': 'purple',
+        'rosa': 'pink'
+    };
+
+    // Verifica si el color en español tiene un equivalente en inglés
+    var colorInEnglish = colorMap[color];
+    if (colorInEnglish) {
+        document.body.style.backgroundColor = colorInEnglish;
+    } else {
+        console.log('Color no reconocido o soportado');
+    }
+}
+
